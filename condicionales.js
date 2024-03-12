d8452bcda24c7d52c49cf8f590a1cebd925d3f1b
David Marin <davidmaring1997@gmail.com>
2024-03-12 19:07:14 -0400

    adding switch to condicionales

--- condicionales.js
+++ condicionales.js
@@ -40,3 +40,44 @@ if (deporte == "beisbol") {
 } else {
   console.log("No tienes descuento");
 }
+
+// Switch
+
+// switch (expresion) {
+
+switch (deporte) {
+  case "beisbol":
+    console.log("Tu descuento es del 10%");
+    break;
+  case "futbol":
+    console.log("Tu descuento es del 20%");
+    break;
+  case "tenis":
+    console.log("Tu descuento es del 5%");
+    break;
+  case "rugby":
+    console.log("Tu descuento es del 15%");
+    break;
+  default:
+    console.log("No tienes descuento switch");
+    break;
+}
+
+edad = 25;
+
+// Cuando necesitamos evaluar condiciones dentro de los casos, le pasamos true al switch.
+
+switch (true) {
+  case edad >= 18:
+    console.log("Eres mayor de edad");
+    break;
+  case edad < 18:
+    console.log("Eres menor de edad");
+    break;
+  case edad > 55:
+    console.log("Eres adulto mayor");
+    break;
+  default:
+    console.log("No se puede determinar tu edad");
+    break;
+}
