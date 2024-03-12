f18cc58fce35ef8f9818fff1fd1d5df16c78098a
David Marin <davidmaring1997@gmail.com>
2024-03-12 19:48:10 -0400

    loops

---
+++ estructuras_de_datos.js
@@ -0,0 +1,21 @@
+// Arrays
+// Objetos
+
+// Arrays
+
+// Un array es una estructura de datos que nos permite almacenar varios valores en una sola variable.
+// Los valores pueden ser de cualquier tipo de dato.
+// Cada valor tiene una posición o índice que empieza desde el 0.
+// Se declaran con corchetes [].
+// Se pueden inicializar con valores o vacíos.
+// Se pueden acceder a los valores con el índice.
+
+var numeros = [1, 2, 3, 4, 5];
+
+console.log(typeof numeros);
+
+var frutas = ["manzana", "pera", "uva", "fresa"];
+
+console.log(frutas[3]);
+
+var mixto = [1, "hola", true, 3.1416];
